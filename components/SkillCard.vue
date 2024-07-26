b3c734525b8ec6276f96c90b85a0c4b11650f824
Rustam <refractory40@gmail.com>
2024-07-26 11:20:59 +0900

    Corrected and improved the code, also added several blocks

--- components/SkillCard.vue
+++ components/SkillCard.vue
@@ -5,8 +5,7 @@ import {SkillsetItems} from "~/data";
 
 <template>
   <div v-for="skill in SkillsetItems" class="bg-white p-4 rounded-lg shadow-md">
-
-    <Icon name= size="36px" style="color: black"/>
+    <Icon :name=skill.Icon size="36px" style="color: black"/>
     <h4 class="text-2xl text-secondary font-semibold mb-2">{{ skill.Skill }}</h4>
     <p class="text-gray-600"> {{ skill.Description }}</p>
   </div>
