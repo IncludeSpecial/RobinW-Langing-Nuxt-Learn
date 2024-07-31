1a4366f682d3ecc5423b1fc15f957cc30e4ed9d6
Rustam <refractory40@gmail.com>
2024-08-01 00:07:48 +0900

    Added a new section and a little optimization.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 import {resolve} from "node:dns";
 
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/RobinW-Langing-Nuxt-Learn/'
-  },
+
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon', "@nuxt/image"],
   devtools: false,
   shadcn: {
